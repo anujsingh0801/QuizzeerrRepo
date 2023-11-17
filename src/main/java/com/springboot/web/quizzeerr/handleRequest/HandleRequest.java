ea4b27785fff4e10676973c24c15d4d0cba9c19f
Singh <anujsingh@informatica.com>
2023-11-17 12:15:12 +0530

    added APIs to update new Question for sujects

--- src/main/java/com/springboot/web/quizzeerr/handleRequest/HandleRequest.java
+++ src/main/java/com/springboot/web/quizzeerr/handleRequest/HandleRequest.java
@@ -1,12 +1,16 @@
 package com.springboot.web.quizzeerr.handleRequest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -16,21 +20,22 @@ import com.springboot.web.quizzeerr.entity.Subject;
 import com.springboot.web.quizzeerr.repo.subjectRepoService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class HandleRequest {
 	
 	@Autowired
 	subjectRepoService subService;
 	
-	@GetMapping("/")
-	public String testApi() {
-		return "Working Fine";
+	@GetMapping("/test")
+	public String testApi(@RequestParam(value="firstName", required=false) String fn, 
+			@RequestParam(value="lastName", required=false) String ln) {
+		
+		return "Working Fine - " + fn + " " + ln;
 	}
 	
 	@PostMapping("/addSubject")
 	public ResponseEntity createSubject(@RequestBody JsonNode newDetails) {
 		try {
-//			ObjectMapper mapper = new ObjectMapper();
-//			JsonNode newDetails = mapper.readTree(sub);	
 			UpdateHandler up = new UpdateHandler(newDetails);
 			Subject newSub = up.createNewSubject();
 			subService.save(newSub);
@@ -41,18 +46,50 @@ public class HandleRequest {
 		}
 	}
 	
-	@GetMapping("/getSubject")
-	public ResponseEntity getSubject(@RequestBody JsonNode param, @RequestHeader("User") String header) {
+	@PostMapping("/addQuestion")
+	public ResponseEntity addQuestion(@RequestBody JsonNode newQues) {
 		try {
-			System.out.println(header);
-			String subName = param.get("subject").asText();
-			int count = param.get("count").asInt();
+			UpdateHandler up = new UpdateHandler(newQues);
+			String subName = newQues.get("name").asText();
+			Subject sub = subService.findByName(subName);
+			int status = up.addQuestion(sub);
+			if(status == 1) {
+				subService.save(sub);
+				return ResponseEntity.status(HttpStatus.ACCEPTED).body("Success");
+			}
+			else
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+		} catch (Exception ex) {
+			System.out.println("error while parsing Json for adding new questions - " + ex);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null); 
+		}
+	}
+	
+	@GetMapping("/getSubject")
+	public ResponseEntity getSubject(@RequestParam String subName,
+            @RequestParam int count) {
+		try {
+//			String subName = param.get("subject").asText();
+//			int count = param.get("count").asInt();
 			Subject sub = subService.findByName(subName);
 			UpdateHandler up = new UpdateHandler();
 			JsonNode subDetails = up.getSubjectDetails(sub, count);
 			return ResponseEntity.status(HttpStatus.ACCEPTED).body(subDetails);
 		}catch(Exception ex) {
 			System.out.println("error while parsing Json for new subject - " + ex);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null); 
+		}
+	}
+	
+	@GetMapping("/getAllSubject")
+	public ResponseEntity gettAllSubject() {
+		
+		try {
+			List<Subject> subs = subService.findAll();
+			UpdateHandler up = new UpdateHandler();
+			JsonNode subJson = up.getAllSubjects(subs);
+			return ResponseEntity.status(HttpStatus.ACCEPTED).body(subJson);
+		} catch (Exception ex) {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null); 
 		}
 	}
